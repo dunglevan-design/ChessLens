92a187732f42fc193510e86a622d6feade10574c
dunglevan-design <dunglevan2001@gmail.com>
2022-02-03 12:49:50 +0000

    code_challenge for PKCE

--- nativeappreact/components/ContextProviders/AuthContext.tsx
+++ nativeappreact/components/ContextProviders/AuthContext.tsx
@@ -1,35 +1,60 @@
-import React, { useContext, useState } from 'react';
-
+import React, { useContext, useState } from "react";
+import * as Crypto from "expo-crypto";
+import { CryptoDigestAlgorithm } from "expo-crypto";
 
 const Authcontext = React.createContext({
-    user: null,
-    LoginWithLichess: () => {},
-    Logout: () => {},
+  user: null,
+  LoginWithLichess: async() => {},
+  Logout: () => {},
 });
-  
-const AuthContext:React.FC = ({children}) => {
-    // useAuth here
 
-    const [user, setUser] = useState({
-        name: "",
-        id: "",
-    })
+const AuthContext: React.FC = ({ children }) => {
+  // useAuth here
 
-    const LoginWithLichess =  () => {
-        //trigger webbrowser login PKCE flow
-        console.log("sign in with Lichess")
-    }
+  const [user, setUser] = useState({
+    name: "",
+    id: "",
+  });
 
-    const Logout = () => {
-        //invalidate the token
-        console.log("log out");
-    }
-
-    return (
-        <Authcontext.Provider value = {{user: null, LoginWithLichess: LoginWithLichess, Logout : Logout}}>
-            {children}
-        </Authcontext.Provider>
+  const LoginWithLichess = async () => {
+    //trigger webbrowser login PKCE flow
+    console.log("sign in with Lichess");
+    //Create code challenge
+    const code_verifier = Array(128)
+      .fill("0")
+      .map(() =>
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".charAt(
+          Math.random() * 62
+        )
+      )
+      .join("");
+    console.log(code_verifier);
+    const digest = await Crypto.digestStringAsync(
+      CryptoDigestAlgorithm.SHA256,
+      code_verifier,
+      {
+        encoding: Crypto.CryptoEncoding.BASE64,
+      }
     );
+    const code_challenge = digest
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=/g, "");
+    console.log(code_challenge);
+  };
+
+  const Logout = () => {
+    //invalidate the token
+    console.log("log out");
+  };
+
+  return (
+    <Authcontext.Provider
+      value={{ user: null, LoginWithLichess: LoginWithLichess, Logout: Logout }}
+    >
+      {children}
+    </Authcontext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(Authcontext);
