9c777a750b74aaf5d469f898842de226628daaf9
dunglevan-design <dunglevan2001@gmail.com>
2022-02-11 16:41:51 +0000

    add home tabs

--- nativeappreact/Screens/HomeScreen.tsx
+++ nativeappreact/Screens/HomeScreen.tsx
@@ -1,15 +1,87 @@
-import { Center, HStack, Image, Text, View } from "native-base";
-import React from "react";
+import {
+  Box,
+  Center,
+  HStack,
+  Image,
+  Pressable,
+  Text,
+  useColorModeValue,
+  View,
+} from "native-base";
+import React, { useRef } from "react";
+import { Animated, useWindowDimensions, StatusBar } from "react-native";
+import { SceneMap, TabView } from "react-native-tab-view";
 import { useAuth } from "../components/ContextProviders/AuthContext";
+import OnlineRoute from "../components/OnlineRoute";
+
+
+const ComputerRoute = () => (
+  <View flex = {1}/>
+);
+
+const InviteRoute = () => (
+  <View flex = {1}/>
+)
+const renderScene = SceneMap({
+  online: OnlineRoute,
+  computer: ComputerRoute,
+  invite: InviteRoute,
+});
 
 const HomeScreen = ({ navigation }) => {
   const { user } = useAuth();
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "online", title: "ONLINE" },
+    { key: "computer", title: "COMPUTER" },
+    { key: "invite", title: "INVITE"}
+  ]);
+  const RenderTabBar = ({ index, routes }) => {
+
+    return (
+      <Box flexDirection={"row"}>
+        {routes.map((route, i) => {
+          const color =
+            index === i
+              ? useColorModeValue("#fff", "#000")
+              : useColorModeValue("#a1a1aa", "");
+
+          const borderColor = index === i ? "purple.500" : "gray.400"
+          return (
+            <Box
+              borderBottomWidth={3}
+              flex={1}
+              alignItems="center"
+              p="3"
+              key = {i} 
+              borderBottomColor={borderColor}
+            >
+              <Pressable
+                onPress={() => {
+                  setIndex(i);
+                }}
+              >
+                <Animated.Text style={{ color }}>{route.title}</Animated.Text>
+              </Pressable>
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  };
+
   return (
-    <View flex={1} background = "#040033">
-      
-      
+    <View flex={1} background="#040033">
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        renderTabBar={() => <RenderTabBar index={index} routes={routes} />}
+        initialLayout={{ width: layout.width }}
+      />
     </View>
   );
 };
-
 export default HomeScreen;
---
+++ nativeappreact/components/OnlineRoute.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'native-base'
+import React from 'react'
+
+const OnlineRoute = () => {
+  return (
+    <View flex = {1}>
+      <Text color = "#fff" p = {6}>MATCH WITH PEOPLE AROUND THE WORLD</Text>
+    </View>
+  )
+}
+
+export default OnlineRoute
